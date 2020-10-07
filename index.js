e93d00da74899f3edc4ff82e655914ab123e37b4
Aishat Abdulfatah <aishatabdulfatah81@gmail.com>
2020-10-07 12:00:22 +0100

    feat: list of contacts

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
-// console.log('Hello world')
 const express = require('express')
-const { request, response } = require('express')
- 
 const app = express()
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
+const contacts = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
   },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
   },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
   }
 ]
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/contacts/', (req, res) => {
+  res.json(contacts)
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const {id} = request.params
-  notes.filter(note=> note.id ===id)
-
-  response.status(204).end()
-})
-
-app.get('/api/notes/:id', (request, response) => 
-{
-  const {id} = request.params
-  const note = notes.find(note => note.id == parseInt(id) )
-
-  if(!note) return `${id} not found`
-
-  response.json(note)
-})
-
-const PORT = 3030
-app.listen(PORT,() => {
-console.log('App is running on port', PORT)
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log('App is running on port', PORT)
 })
