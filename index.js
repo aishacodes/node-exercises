23e9a16bffcf3616eec7ba0272c93ea9976969ec
aishacodes <aishatabdulfatah81@gmail.com>
2020-10-09 16:40:23 +0100

    feat: rest server

---
+++ index.js
@@ -0,0 +1,46 @@
+//express server
+const express = require('express')
+
+const app = express()
+
+const notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
+
+  app.get('/' , (req , res) => {
+
+    res.send('hello world 😀 ggfgfd')
+    
+    })
+    
+  
+    app.get('/notes/' , (req , res) => {
+
+      res.send(notes)
+      
+      })
+
+    const PORT =3001
+    
+    app.listen(PORT , () => {
+    
+    console.log('App is running on port' , PORT)
+    
+    })
